09b6d6bb894f1f2342d7244656a124b149407523
RGRMstudio <raguiromo.studio@outlook.com>
2026-01-02 13:10:09 -0700

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripeA001 = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  // Use exactly this string to clear the Vercel type error
   apiVersion: '2025-02-24.acacia' as any,
   typescript: true,
 });
