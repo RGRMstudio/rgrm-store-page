1a05046d11d390876b8c9e6913d09cbd926c48a4
RGRMstudio <raguiromo.studio@outlook.com>
2026-01-03 00:55:05 -0700

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
+// We use 'as any' to satisfy the strict Acacia type check in Vercel
 export const stripeA001 = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  // Use exactly this string to clear the Vercel type error
   apiVersion: '2025-02-24.acacia' as any,
   typescript: true,
 });
