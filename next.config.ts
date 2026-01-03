70c6508a1f6f4beabd2fe556e7057bd7d4033ce2
RGRMstudio <raguiromo.studio@outlook.com>
2026-01-03 00:57:22 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'files.cdn.printful.com',
-        port: '',
-        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.raguiromo.store',
       },
     ],
-  },
-  // This helps prevent build errors related to strict linting on Vercel
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
