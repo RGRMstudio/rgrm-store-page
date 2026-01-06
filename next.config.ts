5b66ce8c1b3893dd5cc5eacd332b7bd8c668bdc8
RGRMstudio <raguiromo.studio@outlook.com>
2026-01-06 15:46:48 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    // This allows the build to finish even if there are tiny type warnings
+    ignoreBuildErrors: false, 
+  },
+  eslint: {
+    // Ensures your code is clean before it goes live on raguiromo.store
+    ignoreDuringBuilds: false,
+  },
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'files.cdn.printful.com' },
-      { protocol: 'https', hostname: 'www.raguiromo.store' },
-    ],
+    // Allows you to host images on Vercel efficiently
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
+
 export default nextConfig;
