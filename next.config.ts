ada91fcb14b17f58482d8348e4aa59ef5c863fe5
RGRMstudio <raguiromo.studio@outlook.com>
2026-01-11 08:18:49 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Set to true to ensure the build finishes even with minor warnings
     ignoreBuildErrors: true, 
   },
-  // The 'eslint' block is deleted because it is now illegal in NextConfig
   output: "standalone",
 };
 
