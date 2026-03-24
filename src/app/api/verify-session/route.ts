0db4033f575fc4d2eb69baa193722eb142ee2524
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-03-24 08:24:51 -0600

    Update route.ts

--- src/app/api/verify-session/route.ts
+++ src/app/api/verify-session/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-02-24.clover',
+  apiVersion: '2026-02-24.clover',
 });
 
 export async function GET(req: Request) {
