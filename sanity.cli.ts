4cac0fdf81257eb8e8800a99899d4167fb673958
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-03-24 05:55:44 -0600

    Create sanity.cli.ts

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'u6f5r7g8', // Your ID from the screenshot
+    dataset: 'production'
+  }
+})
