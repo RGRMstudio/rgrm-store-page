bc25127199745ca3d850912703b24e1d0e01d97a
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-03-27 19:53:12 -0600

    Update page.tsx

--- src/app/selection/[[...index]]/page.tsx
+++ src/app/selection/[[...index]]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { NextStudio } from 'next-sanity/studio';
-import config from "@/../sanity.config";
+import config from "../../../sanity.config";
 
 export default function StudioPage() {
   // This renders the entire Sanity CMS inside your Next.js app!
