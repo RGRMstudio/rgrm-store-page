5c73e1667d13203def3ea3cd539e49c7058da325
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-03-28 22:28:41 -0600

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
-import product from './schemas/product';
-import settings from './schemas/settings';
+// Using 4-dots because the config is being called from src/app/selection
+import product from '../../schemas/product';
+import settings from '../../schemas/settings';
 
 export default defineConfig({
   name: 'default',
   title: 'RGRM STUDIO ADMIN',
 
-  // This tries the NEXT_PUBLIC version first, then falls back to the secret version
+  // Fallback logic to ensure it works in both local and Vercel environments
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET!,
 
-  basePath: '/selection', // This is where you will go to edit products
+  basePath: '/selection', 
 
   plugins: [structureTool(), visionTool()],
 
