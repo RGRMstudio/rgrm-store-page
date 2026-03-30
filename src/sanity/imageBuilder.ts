7e37b287eb6c797e3a4786b0d31d0bc83e22317b
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-03-30 13:44:35 +0000

    commit

--- src/sanity/imageBuilder.ts
+++
@@ -1,8 +0,0 @@
-import { createImageUrlBuilder } from '@sanity/image-url'
-import { client } from './client' // Use your existing client config
-
-const builder = createImageUrlBuilder(client)
-
-export function urlFor(source: any) {
-  return builder.image(source).auto('format').fit('max')
-}
