53e9a516cfffb882a308dbc18cd0ed9da7eae3fb
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-03-30 07:41:31 -0600

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,23 +1,22 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
-import { visionTool } from '@sanity/vision';
-// Using 4-dots because the config is being called from src/app/selection
-import product from '../../schemas/product';
-import settings from '../../schemas/settings';
+
+// NO ../../ schemas! Use direct paths to your studio folder
+// @ts-ignore
+import product from './studio/schemaTypes/product';
+// @ts-ignore
+import settings from './studio/schemaTypes/settings';
+// @ts-ignore
+import waitlist from './studio/schemaTypes/waitlist';
 
 export default defineConfig({
   name: 'default',
   title: 'RGRM STUDIO ADMIN',
-
-  // Fallback logic to ensure it works in both local and Vercel environments
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET!,
-
-  basePath: '/selection', 
-
-  plugins: [structureTool(), visionTool()],
-
+  projectId: '055j6pls',
+  dataset: 'production',
+  basePath: '/selection',
+  plugins: [structureTool()],
   schema: {
-    types: [product, settings],
+    types: [product, settings, waitlist],
   },
 });
