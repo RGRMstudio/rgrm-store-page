9b038326e6ca6e8ef3b24be9d21cafd49446d82f
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-04-13 08:31:54 +0000

    FIX: Correct sanity.config import path and pin next-sanity version

--- src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { NextStudio } from 'next-sanity/studio';
-import config from "../../../../sanity.config";
-
-export default function StudioPage() {
-  // This renders the entire Sanity CMS inside your Next.js app!
-  return <NextStudio config={config} />;
-}
