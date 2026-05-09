025d04ab792f48d6ed35564f03fd5ed289fa46b8
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-05-09 17:56:33 -0600

    Disable StudioPage and show disabled message

--- src/app/_admin_disabled/[[...index]]/page.tsx
+++ src/app/_admin_disabled/[[...index]]/page.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { NextStudio } from 'next-sanity/studio';
-import config from '../../../../sanity.config';
-
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return <div>Studio is currently disabled.</div>;
 }
