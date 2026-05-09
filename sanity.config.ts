bb7c3849b3b65348c267169a8417634b046c1602
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-05-09 17:45:13 -0600

    Refactor sanity.config.ts for better configuration
    Refactor sanity.config.ts to use variables for projectId and dataset.

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,17 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {schema} from './src/sanity/schema'
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { schema } from './src/sanity/schema';
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'y7y2atv1';
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 
 export default defineConfig({
   name: 'default',
   title: 'RGRM Store',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: projectId,
+  dataset: dataset,
   plugins: [structureTool()],
-  schema: schema,
-})
+  schema: {
+    types: schema.types,
+  },
+});
