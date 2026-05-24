5103209a8476edf6a311c2be9b3495dbfaddf724
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-05-24 04:06:22 -0600

    Update next.config.js for image handling
    Added remote image patterns and formats to nextConfig.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.cdn.printful.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 module.exports = nextConfig;
