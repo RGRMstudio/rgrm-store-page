134a5e9635240eb639d03b425e67cad3a65f88c6
RGRMstudio <241375045+RGRMstudio@users.noreply.github.com>
2026-07-15 15:17:18 -0600

    Update import path for Sanity client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { client } from '@/sanity/client';
+import { client } from '@/sanity/lib/client';
 import Hero from '@/components/Hero';
 import ProductGrid from '@/components/ProductGrid';
 
